439445f3e1ef1f8501b505f9e01d616fe0047a8c
crodriguez19507-prog <crodriguez19507@gmail.com>
2026-09-24 23:35:11 -0500

    Add console log for Cafetería system loading

--- js/app.js
+++ js/app.js
@@ -1,1 +1,2 @@
-// TODO: (opcional) tu JavaScript. Basta con una instrucción.
+// Script básico para cumplir la rúbrica (B)
+console.log("Sistema de Cafetería cargado correctamente por Camilo Rodriguez.");// TODO: (opcional) tu JavaScript. Basta con una instrucción.
